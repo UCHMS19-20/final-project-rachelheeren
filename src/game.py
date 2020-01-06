c3b78a6d931c3faace36f3e99fb52b2a9f9828ae
Rachel Heeren <rheeren@ucvts.org>
2020-01-06 08:11:47 -0500

    game.py outline

--- src/game.py
+++ src/game.py
@@ -1,7 +1,7 @@
-import time
-
-print("Welcome to the sample repository!")
-time.sleep(1)
-print("All the code for your game should go in the src folder")
-time.sleep(1)
-print("Now delete this file and make your own!")
+#Layout of game
+#start in auditorium (level 1)
+#each year is new level
+#will have sleep, studying, eating(cause we know no one actually does that during lunch) as powerups
+#will have tests, projects, principals as obstacles
+#will have grades, mental breakdowns as hits
+#make it all the way through high school without dying and earn the most procrastination point
